9449299e0e344883dcf3cf4ddab6dfe00765c8fd
Tintiwut Yenphet <lolerko15@gmail.com>
2025-03-21 15:11:18 +0700

    Edit A8

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Home from './components/Home';
 import Overall from './components/Overall';
 import Location from './components/Location';
 import About from './components/About';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
 function App() {
   const [selectedLocation, setSelectedLocation] = useState('');
@@ -13,7 +13,8 @@ function App() {
     <Router>
       <Navbar setSelectedLocation={setSelectedLocation} />
       <Routes>
-        <Route path="" element={<Home />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Navigate to="/" />} />
         <Route path="/overall" element={<Overall />} />
         <Route path="/location" element={<Location selectedLocation={selectedLocation} />} />
         <Route path="/about" element={<About />} />
